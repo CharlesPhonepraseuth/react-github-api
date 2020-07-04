2dfb4073f2bf50f0ae648e48f68765b2d8c0422f
CharlesPhonepraseuth <charles.phonepraseuth@gmail.com>
2020-07-04 23:18:42 +0200

    import du React-modele

---
+++ src/components/App/index.js
@@ -0,0 +1,17 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+import './styles.css';
+
+// == Composant
+const App = () => {
+  return (
+    <div className="app">
+      Hello World !
+    </div>
+  );
+};
+
+// == Export
+export default App;
