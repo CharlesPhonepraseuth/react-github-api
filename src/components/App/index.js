bd37bfc5a2028055746f019f2a17ac5ec2902e8a
CharlesPhonepraseuth <charles.phonepraseuth@gmail.com>
2020-07-05 15:42:08 +0200

    Affichage du logo dans le Header

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -7,11 +7,13 @@ import Header from 'src/components/Header';
 import SearchBar from 'src/components/SearchBar';
 import Results from 'src/components/Results';
 
+import githubLogo from 'src/assets/images/logo-github.png';
+
 // == Composant
 const App = () => {
   return (
     <div className="app">
-      <Header />
+      <Header logo={githubLogo} />
       <SearchBar />
       <Results />
     </div>
